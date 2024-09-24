f45d05ec94840dd1acf6bbc52a69637eaf4dcd26
Hans Oskar Trolla <146342767+hansotTLU@users.noreply.github.com>
2024-09-24 19:17:04 +0300

    fix: fix example.routes.js

--- 2024-09-23/Typhoon/Backend/routes/example.routes.js
+++ 2024-09-23/Typhoon/Backend/routes/example.routes.js
@@ -1,32 +1,27 @@
-// router.get("/about", (req, res) => {
-//   res.send("about");
-// });
+const express = require("express");
+const router = express.Router();
 
-// router.get("/ab+cd", (req, res) => {
-//   res.send("ab+cd");
-// });
+router.get("/0*0", (req, res) => {
+  res.send("my first endpoint, 0*0");
+});
 
-// router.get(/a/, (req, res) => {
-//   res.send("/a/");
-// });
+router.get("/users/:userId/books/:bookId", (req, res) => {
+  res.send(req.params);
+});
 
-// router.get("/users/:userId/books/:bookId", (req, res) => {
-//   res.send(req.params);
-// });
+const postMiddleware = (req, res, next) => {
+  console.log("the response will be sent by the next function ...");
+  next();
+};
 
-// const postMiddleware = (req, res, next) => {
-//   console.log("the response will be sent by the next function ...");
-//   next();
-// };
+const postHandler = (req, res) => {
+  res.send(req.params);
+};
 
-// const postHandler = (req, res) => {
-//   res.send(req.params);
-// };
+router.get("/posts/:postID", postMiddleware, postHandler);
 
-// router.get("/posts/:postID", postMiddleware, postHandler);
+router.get("/flights/:from-:to", (req, res) => {
+  res.send(req.params);
+});
 
-// router.get("/flights/:from-:to", (req, res) => {
-//   res.send(req.params);
-// });
-
-// module.exports = router;
+module.exports = router;
